613c0fc6689b7e341841948382c4f8e400bc0bc3
Mostafa Essa <mostafa.essa.ahmed755@gmail.com>
2025-07-26 04:01:15 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
     },
   },
   server: {
@@ -15,16 +15,17 @@ export default defineConfig({
     host: true,
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['vue', 'vue-router', 'vue-i18n'],
-          utils: ['@vueuse/core', 'pinia'],
+          vendor: ["vue", "vue-router", "vue-i18n"],
+          utils: ["@vueuse/core", "pinia"],
         },
       },
     },
   },
-}) 
+  base: "/portfolio/",
+});
